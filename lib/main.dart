09e25551c80a8d9803df3717fc405c69da4262b2
garamkim83 <garamk83@gmail.com>
2022-02-07 01:07:47 +0900

    changed some details
    added a new dart file and changed the width and the margin of the question

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,66 @@
 //import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+
+    print(_questionIndex);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First app'),
+        ),
+        body: Column(
+          children: [
+            //혹은 Text(questions.elementAt(0)),
+            //Text(questions[_questionIndex]),
+            Question(questions[_questionIndex]),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 3'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('Answer 4 chosen!'),
+              child: Text('Answer 4'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
